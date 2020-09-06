627c3089aca4d7e0b3eb2fb38bfff4a969a84de4
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-09-06 12:47:51 +0200

    Updated entrypoint for absolute path reference and binding gunicorn with unix socket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-# gunicorn --bind=0.0.0.0:5001 --threads=25 --workers=2 api:app
-gunicorn --bind unix:api.sock --threads=25 --workers=2 api:app
+# gunicorn --bind=0.0.0.0:5000 --threads=25 --workers=2 api:app
+gunicorn --bind unix:/opt/api/app.sock --threads=25 --workers=2 -m 007 api:app
