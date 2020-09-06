aa3ac131690deacf9e71b6cbf3747c18c95e76e6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-09-06 16:32:34 +0200

    Updated entrypoint to handle development and production environments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-set -e
 
-# gunicorn --bind=0.0.0.0:5000 --threads=25 --workers=2 api:app
-gunicorn --bind unix:/opt/api/app.sock --threads=25 --workers=2 -m 007 api:app
+if [ "${ENV}" = "devl" ]
+then
+    /usr/local/bin/gunicorn --bind=0.0.0.0:5000 --threads=25 --workers=2 api:app
+    echo "[INFO] Gunicorn started"
+fi
+
+if [ "${ENV}" = "prod" ]
+then
+    /usr/local/bin/gunicorn --bind unix:/opt/api/app.sock --threads=25 --workers=2 -m 007 api:app
+    echo "[INFO] Gunicorn started"
+fi
+
+exec "$@"
