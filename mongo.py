e332d81974cba0a64ba108e6efcf6783b88fe0c6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-09-09 19:42:46 +0200

    Updated mongo CRUD library

--- mongo.py
+++ mongo.py
@@ -1,15 +1,26 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 from pymongo import MongoClient
+from bson.json_util import dumps
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+db_connection = os.environ.get('DB_CONNECTION')
+mongo_username = os.environ.get('MONGODB_USER')
+mongo_password = os.environ.get('MONGODB_PASS')
+mongo_database = os.environ.get('MONGO_DB_NAME')
 
 class MongoAPI:
     def __init__(self):
         self.client = MongoClient(
-            'mongodb://{}:27017/'.format(os.environ['DB_CONNECTION']),
-            username='{}'.format(os.environ['MONGO_USERNAME']),
-            password='{}'.format(os.environ['MONGO_PASSWORD']))
+            'mongodb://{}:27017/'.format(db_connection),
+            username='{}'.format(mongo_username),
+            password='{}'.format(mongo_password))
 
-        database = '{}'.format(os.environ['MONGO_DB_NAME'])
-        collection = '{}-collection'.format(os.environ['MONGO_DB_NAME'])
+        database = '{}'.format(mongo_database)
+        collection = '{}-collection'.format(mongo_database)
         cursor = self.client[database]
         self.collection = cursor[collection]
 
@@ -22,13 +33,12 @@ class MongoAPI:
     
     def read_all_items(self):
         items = self.collection.find()
-        output = []
-        if None in items or {} in items:
-            return output
-        
+        output = {}
         for item in items:
-            print(item)
-            output.append({item: item})
+            if "item" not in output:
+                output["item"] = dumps(item)
+            else:
+                output["item"].append(dumps(item))
         return output
 
     def update(self, data, id):
