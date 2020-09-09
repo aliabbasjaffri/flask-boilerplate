3b60c0cb93795dc8a43ea6fd781b851db7415691
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-09-10 00:12:11 +0200

    Updated copy commands to move

--- setup.sh
+++ setup.sh
@@ -5,8 +5,7 @@ apt install -yq python3 python3-pip nginx
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 cd ..
-cp -R flask-boilerplate /opt/api/
-rm -rf flask-boilerplate
+mv flask-boilerplate /opt/api/
 chown -R www-data:www-data /opt/api
 chmod -R g+w /opt/api
 mv /opt/api/api.service /etc/systemd/system/
@@ -14,7 +13,7 @@ systemctl daemon-reload
 systemctl start api.service
 systemctl enable api.service
 systemctl status api.service
-cp /opt/api/nginx.conf /etc/nginx/sites-available/api
+mv /opt/api/nginx.conf /etc/nginx/sites-available/api
 ln -s /etc/nginx/sites-available/api /etc/nginx/sites-enabled
 nginx -t
 systemctl restart nginx
