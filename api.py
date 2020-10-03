5990b31aa6f35961dbf093f5f0314adbb04a8651
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-10-03 12:18:43 +0200

    :art: Updated api for cosmetique changes suggested by linter

--- api.py
+++ api.py
@@ -1,26 +1,31 @@
 from mongo import MongoAPI
-from flask import Flask, Response, request, json, abort, render_template
+from flask import Flask, Response, request, json, render_template
 
 app = Flask(__name__)
+
 
 # default sanity tester
 @app.route('/hello')
 def hello():
-  return render_template('hello.html'), 200
+    return render_template('hello.html'), 200
+
 
 @app.route('/')
 def base():
     return Response(
-        response=json.dumps({'Status': 'Up and running!'}), 
-        status=200, 
+        response=json.dumps({
+            'Status': 'Up and running!'}),
+        status=200,
         mimetype='application/json')
+
 
 # Update the route to your addressing scheme
 @app.route('/<route>', methods=['POST'])
 def insert():
     if request.json is None or request.json == {}:
         return Response(
-            response=json.dumps({'Error': 'Please provide correct/complete information'}),
+            response=json.dumps({
+                'Error': 'Please provide correct/complete information'}),
             status=400,
             mimetype='application/json')
 
@@ -42,22 +47,23 @@ def read():
         mimetype='application/json')
 
 
-
 @app.route('/<route>/<:id>', methods=['PUT'])
 def update(id):
     if request.json is None or request.json == {}:
         return Response(
-            response=json.dumps({'Error': 'Please provide data to update'}),
+            response=json.dumps({
+                'Error': 'Please provide data to update'}),
             status=400,
             mimetype='application/json')
     elif id < 0:
         return Response(
-            response=json.dumps({'Error': 'Please provide correct tab id'}),
+            response=json.dumps({
+                'Error': 'Please provide correct tab id'}),
             status=400,
             mimetype='application/json')
-    
+
     response = MongoAPI().update(request.json, id)
-    
+
     return Response(
         response=json.dumps(response),
         status=200,
@@ -68,10 +74,11 @@ def update(id):
 def delete(id):
     if id < 0:
         return Response(
-            response=json.dumps({'Error': 'Please provide correct item id'}),
+            response=json.dumps({
+                'Error': 'Please provide correct item id'}),
             status=400,
             mimetype='application/json')
-    
+
     response = MongoAPI().delete(id)
 
     return Response(
@@ -79,9 +86,11 @@ def delete(id):
         status=200,
         mimetype='application/json')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     # Remove port=5000 and debug=True for running
