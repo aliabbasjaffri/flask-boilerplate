1b0c8c1b203fe9c59b4ed18866f9e51bd5d7703c
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2020-10-07 23:15:22 +0200

    Added gunicorn conf file for easy configuration

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+from multiprocessing import cpu_count
+
+errorlog = '/var/log/gunicorn/gunicorn.log'
+loglevel = 'warning'
+bind = 'unix:/opt/api/app.sock'
+workers = cpu_count() * 2 + 1
+threads = 2
+user = 'www-data'
+group = 'www-data'
+# Daemon turned off because of the note on the URL
+# https://docs.gunicorn.org/en/latest/deploy.html#monitoring
+daemon = False
